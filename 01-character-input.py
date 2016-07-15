259302e16e5d57b98000b87e4d40d3418d7dc4f4
Jan Chwiejczak <jchwiejczak@gmail.com>
2016-07-15 14:44:52 +0100

    solution using python time module

---
+++ 01-character-input.py
@@ -0,0 +1,11 @@
+from time import gmtime, strftime
+
+name = raw_input('Please enter your name: ')
+age = int(raw_input('How old are you? '))
+times = int(raw_input('How many times would you like to hear when you turn 100? '))
+
+year = int(strftime('%Y', gmtime()))
+turns100 = year + (100 - age)
+
+for i in range(0,times):
+    print('{} you will turn 100 years old in {}!'.format(name,turns100))
