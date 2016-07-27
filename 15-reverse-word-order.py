b9420f6c21736e9be7070d8d264bfb37f60737ca
Jan Chwiejczak <jchwiejczak@gmail.com>
2016-07-27 10:49:58 +0100

    one liner

---
+++ 15-reverse-word-order.py
@@ -0,0 +1,14 @@
+"""
+Write a program (using functions!) that asks the user for a long string containing multiple words. Print back to the user the same string, except with the words in backwards order. For example, say I type the string:
+
+  My name is Michele
+Then I would see the string:
+
+  Michele is name My
+shown back to me.
+"""
+
+def reverse_sentance(sentance):
+    return ' '.join(sentance.split(' ')[::-1])
+
+print(reverse_sentance('they are force of side dark'))
